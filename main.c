7df7583b94deb44241ded8062a1eb1ee170d0e16
Sveva Mazzari <smazzari88@gmail.com>
2023-12-29 19:34:01 +0100

    done cleaning up main - freeing - norminette

--- main.c
+++ main.c
@@ -1,19 +1,95 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "philo.h"
 
-// int check_argv(t_philo ptr)
-// {
-// 	if (ptr.n_philos == 0)
-// 		return (0);
-// 	else if (ptr.time_to_die == 0)
-// 		return (0);
-// 	else if (ptr.time_to_eat == 0)
-// 		return (0);
-// 	else if (ptr.time_to_sleep == 0)
-// 		return (0);
-// 	return (1);
-// }
+t_philo	**fill_info(t_info *ptr, char **argv, int argc);
+int		get_time_s(t_philo **array, t_info *info);
+int		check_status(t_philo *element, struct timeval time_stamp,
+			int i, int *count_done_eating);
+int		is_alive(t_philo *ptr, struct timeval current_time,
+			unsigned long time_die);
+
+int	main(int argc, char **argv)
+{
+	t_philo			**array;
+	t_info			info;
+
+	if (argc == 5 || argc == 6)
+	{
+		array = fill_info(&info, argv, argc);
+		if (array == NULL)
+			return (1);
+		if (create_threads(&info, array) == 1)
+		{
+			free_array(array, info.n_philo);
+			return (1);
+		}
+		while (1)
+		{
+			if (get_time_s(array, &info) == 1 || get_time_s(array, &info) == 2)
+			{
+				free_array(array, info.n_philo);
+				return (1);
+			}
+			usleep(5000);
+		}
+	}
+	else
+		printf("Error, something went wrong!\n");
+}
+
+t_philo	**fill_info(t_info *ptr, char **argv, int argc)
+{
+	t_philo	**array;
+
+	if (argc == 6)
+		ptr->count_max_eat = ft_atoi(argv[5]);
+	else
+		ptr->count_max_eat = -1;
+	ptr->n_philo = ft_atoi(argv[1]);
+	ptr->time_to_die = ft_atoi(argv[2]);
+	ptr->time_to_eat = ft_atoi(argv[3]);
+	ptr->time_to_sleep = ft_atoi(argv[4]);
+	array = (t_philo **)malloc(sizeof(t_philo *) * ptr->n_philo);
+	if (array == NULL)
+		return (NULL);
+	return (array);
+}
+
+int	get_time_s(t_philo **array, t_info *info)
+{
+	int				i;
+	int				count_done_eating;
+	struct timeval	time;
+
+	i = 0;
+	count_done_eating = 0;
+	gettimeofday(&time, NULL);
+	while (i < info->n_philo)
+	{
+		if (check_status(array[i], time, i, &count_done_eating) == 1)
+			return (1);
+		i++;
+	}
+	if (count_done_eating == info->n_philo)
+		return (2);
+	return (0);
+}
+
+int	check_status(t_philo *element, struct timeval time_stamp,
+					int i, int *count_done_eating)
+{
+	unsigned long	time_in_ms;
+
+	if (element->least_eating_status == DONE_EATING)
+		*count_done_eating = *count_done_eating + 1;
+	if (element->status == NOT_EATING
+		&& is_alive(element, time_stamp, element->info->time_to_die) == DEAD)
+	{
+		time_in_ms = time_stamp.tv_sec * 1000 + time_stamp.tv_usec / 1000;
+		printf("%lu philosoper %d has died\n", time_in_ms, i + 1);
+		return (1);
+	}
+	return (0);
+}
 
 int	is_alive(t_philo *ptr, struct timeval current_time, unsigned long time_die)
 {
@@ -36,96 +112,5 @@ int	is_alive(t_philo *ptr, struct timeval current_time, unsigned long time_die)
 		/ 1000;
 	if (total_difference > time_die)
 		return (1);
-	return (0);
-}
-
-void	fill_info(t_info *ptr, char **argv, int argc)
-{
-	if (argc == 6)
-		ptr->count_max_eat = ft_atoi(argv[5]);
-	else
-		ptr->count_max_eat = -1;
-	ptr->n_philo = ft_atoi(argv[1]);
-	ptr->time_to_die = ft_atoi(argv[2]);
-	ptr->time_to_eat = ft_atoi(argv[3]);
-	ptr->time_to_sleep = ft_atoi(argv[4]);
-}
-
-int	check_status(t_philo *element, struct timeval time_stamp,
-					int i, int *count_done_eating)
-{
-	unsigned long	time_in_ms;
-
-	if (element->least_eating_status == DONE_EATING)
-		*count_done_eating = *count_done_eating + 1;
-	if (element->status == NOT_EATING
-		&& is_alive(element, time_stamp, element->info->time_to_die) == DEAD)
-	{
-		time_in_ms = time_stamp.tv_sec * 1000 + time_stamp.tv_usec / 1000;
-		printf("%lu philosoper %d has died\n", time_in_ms, i + 1);
-		return (1);
-	}
-	return (0);
-}
-
-int	get_time_stamp_last_eaten(t_philo **array, t_info *info)
-{
-	int				i;
-	int				count_done_eating;
-	struct timeval	time;
-
-	i = 0;
-	count_done_eating = 0;
-	gettimeofday(&time, NULL);
-	while (i < info->n_philo)
-	{
-		if (check_status(array[i], time, i, &count_done_eating) == 1)
-			return (1);
-		i++;
-	}
-	if (count_done_eating == info->n_philo)
-		return (2);
-	return (0);
-}
-
-void 	free_array(t_philo **array, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-	{
-		free(array[i]);
-		i++;
-	}
-	free(array);
-}
-
-int	main(int argc, char **argv)
-{
-	t_philo			**array;
-	t_info			info;
-	int				result;
-
-	if (argc == 5 || argc == 6)
-	{
-		fill_info(&info, argv, argc);
-		array = (t_philo **)malloc(sizeof(t_philo *) * info.n_philo);
-		if (array == NULL)
-			return (1);
-		create_threads(&info, array);
-		while (1)
-		{
-			result = get_time_stamp_last_eaten(array, &info);
-			if (result == 1 || result == 2)
-			{
-				free_array(array, info.n_philo);
-				return (1);
-			}
-			usleep(5000);
-		}
-	}
-	else
-		printf("Error, something went wrong!\n");
 	return (0);
 }
